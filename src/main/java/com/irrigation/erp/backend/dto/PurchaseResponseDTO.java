ca5c58e3b08dc55be198940c70c2c22e2cd1dbd0
mihiranwijesekara <achinthamihiran654@gmail.com>
2025-08-20 12:10:24 +0530

    develop purchaseRequest table Get method add pending

--- src/main/java/com/irrigation/erp/backend/dto/PurchaseResponseDTO.java
+++ src/main/java/com/irrigation/erp/backend/dto/PurchaseResponseDTO.java
@@ -1,25 +1,23 @@
 package com.irrigation.erp.backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.irrigation.erp.backend.model.PurchaseRequest;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
 
+@Getter
+@AllArgsConstructor
+@ToString
 public class PurchaseResponseDTO {
     private Long id;
     private String requestedByName;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDateTime requestedAt;
+
     private String itemName;
-
-    public PurchaseResponseDTO(Long id, String requestedByName, LocalDateTime requestedAt, String itemName) {
-        this.id = id;
-        this.requestedByName = requestedByName;
-        this.requestedAt = requestedAt;
-        this.itemName = itemName;
-    }
-
-    public Long getId() { return id; }
-    public String getRequestedByName() { return requestedByName; }
-    public LocalDateTime getRequestedAt() { return requestedAt; }
-    public String getItemName() { return itemName; }
+    private PurchaseRequest.Status status;
 }
